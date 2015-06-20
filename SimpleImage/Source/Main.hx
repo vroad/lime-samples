2855d0003d0b13449467a44945ef009aca8b1814
vroad <xvroad@gmail.com>
2015-06-21 00:11:27 +0900

    Remove unnecessary #if js

--- SimpleImage/Source/Main.hx
+++ SimpleImage/Source/Main.hx
@@ -138,11 +138,7 @@ class Main extends Application {
 					gl.bindTexture (gl.TEXTURE_2D, texture);
 					gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 					gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-					#if js
-					gl.texImage2D (gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image.src);
-					#else
 					gl.texImage2D (gl.TEXTURE_2D, 0, gl.RGBA, image.buffer.width, image.buffer.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, image.data);
-					#end
 					gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 					gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 					gl.bindTexture (gl.TEXTURE_2D, null);
